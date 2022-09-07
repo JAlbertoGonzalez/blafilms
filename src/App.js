16c982d613b4d0c5e97557cad8896746414cb48a
Alberto Torre <45895462+JAlbertoGonzalez@users.noreply.github.com>
2022-09-07 14:08:40 +0200

    P2: pagination

--- src/App.js
+++ src/App.js
@@ -6,19 +6,30 @@ import { ReactComponent as ChevronRight } from './chevron-right.svg'
 
 function App() {
   const [searchInput, setSearchInput] = useState('')
+  const [searchPage, setSearchPage] = useState(1)
   const [searchResult, setSearchResult] = useState()
 
-  const search = async () => {
-    if (!searchInput) return []
+  const search = async (input = searchInput, page = 1) => {
+    if (!input) return null
 
     const response = await fetch(
-      'http://www.omdbapi.com/?apikey=a461e386&s=' + searchInput,
-    )
+      'http://www.omdbapi.com/?apikey=a461e386&s=' + input + '&page=' + page,
+    ).catch(() => null)
 
     const data = await response.json()
 
-    setSearchResult(data)
+    setSearchResult(data.Search ? data : null)
   }
+
+  const handleNewSearch = () => {
+    // Reset to first page
+    setSearchPage(1)
+    search()
+  }
+
+  useEffect(() => {
+    search(searchInput, searchPage)
+  }, [searchPage])
 
   return (
     <div className="App">
@@ -28,13 +39,13 @@ function App() {
           placeholder="Search..."
           onChange={x => setSearchInput(x.target.value)}
         />
-        <button onClick={search}>Search</button>
+        <button onClick={handleNewSearch}>Search</button>
       </div>
       {!searchResult ? (
         <p>No results yet</p>
       ) : (
         <div className="search-results">
-          <div className="chevron">
+          <div className="chevron" onClick={() => setSearchPage(Math.max(searchPage - 1, 0))}>
             <ChevronLeft />
           </div>
           <div className="search-results-list">
@@ -51,7 +62,7 @@ function App() {
               </div>
             ))}
           </div>
-          <div className="chevron">
+          <div className="chevron" onClick={() => setSearchPage(searchPage + 1)}>
             <ChevronRight />
           </div>
         </div>
