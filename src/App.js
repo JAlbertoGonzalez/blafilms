3b05db5f25154c316007c1ee16ae359e7f61bab0
Alberto Torre <45895462+JAlbertoGonzalez@users.noreply.github.com>
2022-09-08 08:09:57 +0200

    App.js to page component

--- src/App.js
+++ src/App.js
@@ -1,78 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './App.css'
-import { ReactComponent as ChevronLeft } from './images/chevron-left.svg'
-import { ReactComponent as ChevronRight } from './images/chevron-right.svg'
-import MovieCard from './components/MovieCard'
-import SearchBar from './components/SearchBar'
-import { omdbSearch } from './lib/omdbapi'
+import SearchMovies from './pages/SearchMovies'
 
 function App() {
-  const [searchInput, setSearchInput] = useState('')
-  const [searchPage, setSearchPage] = useState(1)
-  const [searchResult, setSearchResult] = useState()
-  const [loading, setLoading] = useState(false)
-
-  const handleSearch = () => {
-    // Reset to first page
-    setLoading(true)
-    omdbSearch(searchInput, searchPage)
-      .then(setSearchResult)
-      .finally(() => setLoading(false))
-  }
-
-  const handleNewSearch = () => {
-    setSearchPage(1)
-    handleSearch()
-  }
-
-  const nextPageExists = () => {
-    return (
-      10 * (searchPage - 1) + searchResult.Search.length <
-      searchResult.totalResults
-    )
-  }
-
-  useEffect(() => {
-    handleSearch()
-  }, [searchPage])
-
   return (
     <div className="App">
-      <SearchBar
-        onChange={e => setSearchInput(e.target.value)}
-        onSubmit={handleNewSearch}
-        loading={loading}
-      />
-
-      {!searchResult || loading ? (
-        <p className="no-results">{loading ? 'Loading' : 'No results'}</p>
-      ) : (
-        <div className="search-results">
-          <div className="chevron">
-            {searchPage > 1 && (
-              <button
-                onClick={() => setSearchPage(Math.max(searchPage - 1, 1))}
-              >
-                {' '}
-                <ChevronLeft />
-              </button>
-            )}
-          </div>
-
-          <div className="search-results-list">
-            {searchResult.Search.map(result => (
-              <MovieCard key={result.imdbID} {...result} />
-            ))}
-          </div>
-          <div className="chevron">
-            {nextPageExists() && (
-              <button onClick={() => setSearchPage(searchPage + 1)}>
-                <ChevronRight />
-              </button>
-            )}
-          </div>
-        </div>
-      )}
+      <SearchMovies />
     </div>
   )
 }
---
+++ src/pages/SearchMovies.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react'
+import { omdbSearch } from '../lib/omdbapi'
+import { ReactComponent as ChevronLeft } from '../images/chevron-left.svg'
+import { ReactComponent as ChevronRight } from '../images/chevron-right.svg'
+import MovieCard from '../components/MovieCard'
+import SearchBar from '../components/SearchBar'
+
+export default function SearchMovies() {
+  const [searchInput, setSearchInput] = useState('')
+  const [searchPage, setSearchPage] = useState(1)
+  const [searchResult, setSearchResult] = useState()
+  const [loading, setLoading] = useState(false)
+
+  const handleSearch = () => {
+    setLoading(true)
+    omdbSearch(searchInput, searchPage)
+      .then(setSearchResult)
+      .finally(() => setLoading(false))
+  }
+
+  const handleNewSearch = () => {
+    // Reset to first page
+    setSearchPage(1)
+    handleSearch()
+  }
+
+  const nextPageExists = () => {
+    return (
+      10 * (searchPage - 1) + searchResult.Search.length <
+      searchResult.totalResults
+    )
+  }
+
+  useEffect(() => {
+    handleSearch()
+  }, [searchPage])
+
+  return (
+    <>
+      <SearchBar
+        onChange={e => setSearchInput(e.target.value)}
+        onSubmit={handleNewSearch}
+        loading={loading}
+      />
+
+      {!searchResult || loading ? (
+        <p className="no-results">{loading ? 'Loading' : 'No results'}</p>
+      ) : (
+        <div className="search-results">
+          <div className="chevron">
+            {searchPage > 1 && (
+              <button
+                onClick={() => setSearchPage(Math.max(searchPage - 1, 1))}
+              >
+                {' '}
+                <ChevronLeft />
+              </button>
+            )}
+          </div>
+
+          <div className="search-results-list">
+            {searchResult.Search.map(result => (
+              <MovieCard key={result.imdbID} {...result} />
+            ))}
+          </div>
+          <div className="chevron">
+            {nextPageExists() && (
+              <button onClick={() => setSearchPage(searchPage + 1)}>
+                <ChevronRight />
+              </button>
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
